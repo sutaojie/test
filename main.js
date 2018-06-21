5940331e0caad5d0aa68b7c991c3a156c23115a5
sutaojie <sutaojie@gmail.com>
2018-06-21 23:26:43 +0800

    三角形判断

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-var isSquare = function(n){
-  if(n < 0){
-    return false;
-  }else if(true){
-  	let temp = Math.sqrt(n);
-  	//正整数判断
-  	if(~~temp === temp){
-  		return true;
-  	}else{
-  		return false;
-  	}
+function isTriangle(a,b,c)
+{
+  let l = a+b+c;
+  if(l <= 0){
+   return false;
+  }else if((l-a >a)&&(l-b >b)&&(l-c >c)){
+    return true;
+  }else{
+  	return false;
   }
 }
 
-console.log(isSquare(16));
+console.log(isTriangle(1,4,2));
